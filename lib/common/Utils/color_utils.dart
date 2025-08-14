c033ac8be49480b37abde7066815d67d0177bd61
BernardMugita <mugita68@gmail.com>
2025-08-14 23:14:11 +0300

    feat: color-utils configuration for global coloring

---
+++ lib/common/Utils/color_utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorUtils {
+  static const Color primaryColor = Color(0xFF74ff6d);
+  static const Color secondaryColor = Color(0xFF008800);
+  static const Color primaryTextColor = Color(0xFFFFFFFF);
+  static const Color secondaryTextColor = Color.fromARGB(255, 0, 0, 0);
+
+  static const Color transparent = Colors.transparent;
+}
