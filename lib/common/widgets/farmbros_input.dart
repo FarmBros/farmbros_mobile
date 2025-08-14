ad861e809de7b993aefee002f9bd667a13149d37
BernardMugita <mugita68@gmail.com>
2025-08-14 23:15:31 +0300

    feat: farmbros global input field with dynamic parameters

---
+++ lib/common/widgets/farmbros_input.dart
@@ -0,0 +1,49 @@
+import 'package:farmbros_mobile/common/Utils/color_utils.dart';
+import 'package:flutter/material.dart';
+
+class FarmbrosInput extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final bool isPassword;
+
+  const FarmbrosInput({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.isPassword,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: TextField(
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: const TextStyle(
+              fontSize: 14, color: ColorUtils.secondaryTextColor),
+          prefixIcon: Icon(
+            icon,
+            color: ColorUtils.secondaryColor,
+          ),
+          suffixIcon: isPassword ? const Icon(Icons.visibility) : null,
+
+          // Outline border always visible
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: ColorUtils.primaryColor, width: 1.5),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: ColorUtils.secondaryColor, width: 2),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: ColorUtils.primaryColor, width: 1.5),
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+}
